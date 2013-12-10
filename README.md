9521ac4dfe3ec9c5425a4eea24aa1540827c966a
Admin <admin@raze.com>
2013-12-09 17:07:56 -0800

    Added Readme
    added editor dependency and
    added warning message

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a small vim plugin to publish and edit posts on the ghost server
+
+Prerequisites
+editor plugin
+
+npm install editor
+
+Clone this repo and run node index.js --help 
--- index.js
+++ index.js
@@ -1,135 +1,166 @@
-var postfile = './post.txt';
-var moment = require('moment');
-var uuid = require('node-uuid');
-var sys = require('sys');
-var exec = require('child_process').exec;
-var Showdown = require('showdown');
-var github = require('../core/shared/vendor/showdown/extensions/github');
-var converter = new Showdown.converter({extensions: [github]});
-var markdown, html;
 var Attributes= [
 	'id', 'uuid', 'title', 'slug', 'markdown', 'html', 'meta_title', 'meta_description',
 	'featured', 'image', 'status', 'language', 'author_id', 'created_at', 'created_by', 'updated_at', 'updated_by',
 	'published_at', 'published_by'
 ];
-var sanitize = require('validator').sanitize;
-
-var usage = ["\n\n\tnode index.js <existing_slug> --edit|--submit\n\n", "\n\n\tnode index <new_slug> --title <title> [--file <file-name>]\n\n"];
+var usage = [
+	"\nTo list the existing posts:\n\tnode index.js\n",
+	"\nTo edit an existing post:\n\tnode index.js <existing_slug>\n",
+	"\nTo create a new post:\n\tnode index.js <new_slug> --title <title>\n",
+	"\nTo view markdown rules:\n\tcat markdown-guide.md\n"
+];
 main();
-//subprocess();
-
-function subprocess () {
-	var spawn = require('child_process').spawn('vim');
-}
 
 function main() {
+	var postfile = '/tmp/post.txt';
+	var moment = require('moment');
+	var Showdown = require('showdown');
+	var github = require('../core/shared/vendor/showdown/extensions/github');
+	var converter = new Showdown.converter({extensions: [github]});
 	var sqlite3 = require('sqlite3').verbose();
+	var uuid = require('node-uuid');
 	var db = new sqlite3.Database('../content/data/ghost-dev.db');
 	var slug = "";
 	var slug_array = {};
 	var	fs = require('fs');
+	var markdown, html;
+	var note_str = "<!-- Use :cq if you don't want to publish it. Remove this line before publishing. -->\n";
 	db.serialize(function() {
-		db.each("SELECT slug FROM posts",
+		db.each("SELECT title,slug FROM posts",
 			function(err, row) {
-				slug_array[row.slug] = true;
+				slug_array[row.slug] = row.title;
 			},
 			function () {
 				if (process.argv[2]) {
+					if (process.argv[2] == "-h" || process.argv[2] == "--help") {
+						console.log(usage[0]+usage[1]+usage[2]+usage[3]);
+						return;
+					}
 					slug = process.argv[2];
+					if (/\s/.test(slug) || /[A-Z]/.test(slug)) {
+						console.log("Slug cannot have white spaces or uppercase letters.");
+						return;
+					}
 					if (slug_array[slug]) {
-						if (process.argv[3] && process.argv[3] == "--edit") {
-							var query = "SELECT markdown FROM posts where slug='"+slug+"'";
-							db.all(query, function (err, row) {
-								if (err) {
-									console.log(err);
-								} else {
-									fs.writeFile(postfile, row[0].markdown, function (err) {
-										if (err) {
-											console.log(err);
-										}
-										console.log("You can now edit the file post.txt and then run\nnode index.js "+slug+" --submit");
-									});		
-									db.close();
-								}
-							});
-						} else if (process.argv[3] && process.argv[3] == "--submit") {
-							fs.readFile(postfile, 'utf8', function (err, data) {
-								if (err) {
-									return sys.puts(err);
-								}
-								var updated_by = 1;
-								var updated_at = moment(new Date()).toDate().getTime();
-								markdown = data;
-								var html = converter.makeHtml(markdown);
-								//console.log(query);
-								db.prepare("UPDATE posts set updated_by=1, updated_at=(?), markdown=(?), html=(?) where slug=(?)")
-								.run(updated_at, markdown, html, slug, function(err) {
-										if(err) {
-											console.log(err);
-										}
-								})
-								.finalize(function (err) {
-									if(err) {
-										console.log(err);
-										db.close();
-									}
-								});
-							});
-						} else {
-							console.log("Slug "+slug+" already exists.");
-							console.log(usage[0]);
-						}
+						copy_post_to_file();
 					} else {
-						if (/\s/.test(slug)) {
-							console.log("Slug cannot have white spaces");
-							return;
-						}
-						if (process.argv[3] && process.argv[3] == "--title") {
-							if (process.argv[4]) {
-								var title = process.argv[4];
-								if (process.argv[5] && process.argv[5] == "--file" && process.argv[6]) {
-									postfile = process.argv[6];
-								} else {
-									console.log("\n\npost.txt is considered for the post\n\n");
-								}
-								var uid = uuid.v4();
-								fs.readFile(postfile, 'utf8', function (err,data) {
-									if (err) {
-										return sys.puts(err);
-									}
-									var created_by = 1;
-									var created_at = moment(new Date()).toDate().getTime();
-									markdown = data;
-									var html = converter.makeHtml(markdown);
-									db.prepare("INSERT INTO posts (uuid, created_at, created_by, published_by, published_at, language, author_id, status, slug, title, markdown, html) VALUES((?), (?), 1, 1, (?), (?), 1, (?), (?), (?), (?), (?))")
-									.run(uid, created_at, created_at, "en_US", "published", slug, title, markdown, html, function(err) {
-										if(err) {
-											console.log(err);
-										}
-									})
-									.finalize(function (err) {
-										if(err) {
-											console.log(err);
-											db.close();
-										}
-									});
-								});
-							} else {
-								//console.log("Slug "+slug+" doesn't exist, a title is required");
-								console.log("Title cannot be empty");
-								console.log(usage[1]);
-							}
+						if (process.argv[3] && process.argv[3] == "--title" && process.argv[4]) {
+							create_empty_post();
 						} else {
-							console.log("Slug "+slug+" doesn't exist, a title is required");
-							console.log(usage[1]);
+							console.log("New post requires a title");
+							console.log(usage[2]);
 						}
 					}
 				} else {
-					for (var key in slug_array) {
-						console.log(key);
-					}
+					show_existing_slugs();
 				}
 			}
 		);
 	});
+	function show_existing_slugs() {
+		for (var key in slug_array) {
+			console.log("\n\tslug => "+key + "\ttitle => "+slug_array[key]);
+		}
+		console.log(" ");
+	}
+	function copy_post_to_file() {
+		var query = "SELECT markdown FROM posts where slug='"+slug+"'";
+		db.all(query, function (err, row) {
+			if (err) {
+				console.log(err);
+				return;
+			} else {
+				str = note_str+row[0].markdown;
+				fs.writeFile(postfile, str, function (err) {
+					if (err) {
+						console.log(err);
+						return;
+					}
+					var editor = require('editor');
+					editor(postfile, function (code, sig) {
+						console.log('Code ' + code+' Signal '+sig);
+						if (code === 0 && typeof signal === 'undefined') {
+							submit_editted_post();
+							console.log('Editted');
+						}
+					});
+				});		
+			}
+		});
+	}
+
+	function create_empty_post() {
+		var str = note_str;
+		fs.writeFile(postfile, str, function (err) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var editor = require('editor');
+			editor(postfile, function (code, sig) {
+				console.log('Code ' + code+' Signal '+sig);
+				if (code === 0 && typeof signal === 'undefined') {
+					console.log('Submitted');
+					submit_new_post();
+				}
+			});
+		});		
+	}
+
+	function submit_editted_post() {
+		fs.readFile(postfile, 'utf8', function (err, data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var updated_by = 1;
+			var updated_at = moment(new Date()).toDate().getTime();
+			markdown = data;
+			var html = converter.makeHtml(markdown);
+			//console.log(query);
+			db.prepare("UPDATE posts set updated_by=1, updated_at=(?), markdown=(?), html=(?) where slug=(?)")
+			.run(updated_at, markdown, html, slug, function(err) {
+				if(err) {
+					console.log(err);
+					return;
+				}
+			})
+			.finalize(function (err) {
+				if(err) {
+					console.log(err);
+					return;
+				}
+				db.close();
+			});
+		});
+	}
+
+	function submit_new_post() {
+		var title = process.argv[4];
+		var uid = uuid.v4();
+		fs.readFile(postfile, 'utf8', function (err,data) {
+			if (err) {
+				console.log(err);
+				return;
+			}
+			var created_by = 1;
+			var created_at = moment(new Date()).toDate().getTime();
+			markdown = data;
+			var html = converter.makeHtml(markdown);
+			db.prepare("INSERT INTO posts (uuid, created_at, created_by, published_by, published_at, language, author_id, status, slug, title, markdown, html) VALUES((?), (?), 1, 1, (?), (?), 1, (?), (?), (?), (?), (?))")
+			.run(uid, created_at, created_at, "en_US", "published", slug, title, markdown, html, function(err) {
+				if(err) {
+					console.log(err);
+					return;
+				}
+			})
+			.finalize(function (err) {
+				if(err) {
+					console.log(err);
+					return;
+				}
+				db.close();
+			});
+		});
+	}
 }
